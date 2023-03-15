6e4c19179691742cbf410080ec3f82a767d4a587
Van-tesh <smalletvicky@gmail.com>
2023-03-16 00:11:07 +0300

    add while loop

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 import random
-word_list= ["Cameroon", "Baboon", "Elephant"]
-word=random.choice(word_list)
+word_list= ["cameroon", "baboon", "elephant", "cow", "dog", "White" ]
 
-guess= input("Guess a random letter:\n").lower()
+chosen_word=random.choice(word_list)
 
-for letter in word:
-  if letter == guess:
-    print("right")
-  else:
-    print("Wrong")
+
+print(chosen_word)
+
+
+
+
+display=[]  
+for letter in chosen_word:
+    display+="_"
+end_of_game=False     
+while not end_of_game :
+   
+  guess= input("Guess a random letter:\n").lower()  
+  
+       
+  for position in range(len(chosen_word)):
+        letter=chosen_word[position]
+        if letter==guess:
+          display[position]=letter
+
+
+  print (display)
+
+  if "_" not in display:
+    end_of_game=True
+    print("You win") 
